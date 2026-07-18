28e57ff0ebbd9475f29fc1ce1d74478f181dbf9c
XAIZ096 <2411874314@qq.com>
2026-07-19 07:56:33 +0800

    Delete server copy directory

--- server copy/src/db/collections.js
+++
@@ -1,8 +0,0 @@
-const { connectToDatabase } = require('./mongoClient');
-
-async function getCollection(collectionName) {
-  const database = await connectToDatabase();
-  return database.collection(collectionName);
-}
-
-module.exports = { getCollection };
--- server copy/src/middleware/requireAuth.js
+++
@@ -1,9 +0,0 @@
-function requireAuth(request, response, next) {
-  if (request.isAuthenticated && request.isAuthenticated()) {
-    return next();
-  }
-
-  return response.status(401).json({ message: 'Authentication is required.' });
-}
-
-module.exports = { requireAuth };
